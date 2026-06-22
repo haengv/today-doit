356a3ef537f244f2b416ece6f175a62b24def716
행 <haeng@haeng-ui-MacBookPro.local>
2026-06-22 19:58:11 +0900

    fix: remove TDSMobileAITProvider to prevent white screen crash on Vercel

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
-import { TDSMobileAITProvider } from "@toss/tds-mobile-ait";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import config from "../granite.config.ts";
 import "./index.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <TDSMobileAITProvider brandPrimaryColor={config.brand.primaryColor}>
-      <App />
-    </TDSMobileAITProvider>
+    <App />
   </StrictMode>,
 );
